27c42a5e9f86d3f5d2322bdcaf4b03dce8c8583a
mohamadgamalali <mohamad.dapsha@gmail.com>
2018-12-22 22:23:46 +0200

    Item-DataBase

--- src/mainpackage/Item_DataBase.java
+++ src/mainpackage/Item_DataBase.java
@@ -3,16 +3,42 @@ package mainpackage;
 import java.util.ArrayList;
 
 public class Item_DataBase {
-	ArrayList<Item> items;
-	
-	public boolean insert(Item item) {
-		items.add(item);
-		return true;
-	}
-	/*
-	**********
-	**********
-	**********
-	*/
-	
+
+    public static ArrayList<Item> items;
+
+    public static boolean insert(Item item) {
+        items.add(item);
+        return true;
+    }
+
+    
+    public static boolean delete(Item item) {
+        for (Item i : items) {
+            if (item.get_id() == i.get_id()) {
+                items.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean update(Item item) {
+        for (Item i : items) {
+            if (item.get_id() == i.get_id()) {
+                i = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Item search(Item item) {
+        for (Item i : items) {
+            if (item.get_id() == i.get_id()) {
+                return i;
+            }
+        }
+        return null;
+    }
+
 }
