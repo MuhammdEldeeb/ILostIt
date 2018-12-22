f01c4508482972e0e427c505eecc7f2d9de02171
Muhammed Eldeeb <meldeeb1998@gmail.com>
2018-12-22 20:40:43 +0200

    create the dataabbase of the post

--- src/mainpackage/Post_DataBase.java
+++ src/mainpackage/Post_DataBase.java
@@ -3,16 +3,39 @@ package mainpackage;
 import java.util.ArrayList;
 
 public class Post_DataBase {
-	ArrayList<Post> posts;
+	public static ArrayList<Post> posts;
 	
-	public boolean insert(Post post) {
+	public static boolean insert(Post post) {
 		posts.add(post);
 		return true;
 	}
-	/*
-	**********
-	**********
-	**********
-	*/
+        
+        public static boolean delete(Post post){
+            for(Post i : posts){
+                if(post.get_id() == i.get_id()){
+                    posts.remove(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public static boolean udate(Post post){
+            for(Post i : posts){
+                if(post.get_id() == i.get_id()){
+                    i = post;
+                    return true;
+                }
+            }
+            return false;
+        }
 	
+        public static Post search(Post post){
+            for(Post i : posts){
+                if(post.get_id() == i.get_id()){
+                    return i;
+                }
+            }
+            return null;
+        }
 }
