a3cd8b44b00dfe2100a025d0dce2c95588345e10
Muhammed Eldeeb <meldeeb1998@gmail.com>
2018-12-22 21:02:49 +0200

    create empty constructor in user class

--- src/mainpackage/Boundray.java
+++ src/mainpackage/Boundray.java
@@ -1,10 +1,25 @@
 package mainpackage;
 
+import java.util.Scanner;
+
 public class Boundray {
 	
-	public static void main(String [] args) {	
+	public static void main(String [] args) {
+                Scanner s = new Scanner(System.in);
 		System.out.println("\t\tWelcome To ILostIt!\n");
-		/*
+		System.out.println("Select the function u want: ");
+                System.out.println("(1) Login\n(2) Sign up");
+                int choice = s.nextInt();
+                switch(choice){
+                    case 1:
+                    {
+                        User user = new User();
+                        
+                        break;
+                    }
+                }
+                
+                /*
 		**********
 		**********
 		**********
